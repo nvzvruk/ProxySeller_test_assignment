d2368a67cc53f77b09cbf36fc7892e821197dc61
Dmytro Nazaruk <nvzvruk@gmail.com>
2024-02-02 14:58:08 +0200

    Add link to users page

--- src/shared/components/AppLink.tsx
+++ src/shared/components/AppLink.tsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 interface AppLinkProps {
   path: string;
+  className?: string;
   children: ReactNode;
 }
 
-export const AppLink = memo<AppLinkProps>(({ children, path }) => {
+export const AppLink = memo<AppLinkProps>(({ children, path, className }) => {
   return (
     <Link
       to={path}
-      className="text-blue-500 hover:underline hover:text-blue-700"
+      className={`text-blue-500 hover:underline hover:text-blue-700 ${className || ""}`}
     >
       {children}
     </Link>
--- src/shared/components/PageHeading.tsx
+++ src/shared/components/PageHeading.tsx
@@ -1,10 +1,22 @@
 import { memo, ReactNode } from "react";
 import { Portal } from "@/shared/components/Portal";
+import { AppLink } from "@/shared/components/AppLink";
+import { RoutePath } from "@/shared/config/routePaths";
+import { useLocation } from "react-router-dom";
 
 export const PageHeading = memo(({ children }: { children: ReactNode }) => {
+  const location = useLocation();
+
   return (
     <Portal element={document.getElementById("pageHeading")}>
-      <h2 className="font-semibold text-white">{children}</h2>
+      <div className="flex w-full gap-2 justify-between">
+        <h2 className="font-semibold text-white">{children}</h2>
+        {location.pathname !== RoutePath.Users && (
+          <AppLink path={RoutePath.Users} className="text-white">
+            &larr; Back to users page
+          </AppLink>
+        )}
+      </div>
     </Portal>
   );
 });
